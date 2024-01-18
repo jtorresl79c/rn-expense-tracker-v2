698ee61210f74593823ac8522bc4f2a7e9d9a4a0
jtorresl <jtorresl79c@gmail.com>
2024-01-17 23:12:06 -0800

    148. Building a Custom Input Component, 149. Creating an Overall Form y 150. Configuring the Form Input Elements

--- screens/ManageExpense.jsx
+++ screens/ManageExpense.jsx
@@ -1,11 +1,12 @@
 import { useLayoutEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
 
 import { useDispatch } from "react-redux";
 import { deleteExpense, addExpense, updateExpense } from "../store/expenses";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -51,6 +52,9 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
+
+      <ExpenseForm/>
+
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>
           Cancel
