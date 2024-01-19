ab8356a530476e5832aab3fab3dfac3afcc9e3ab
jtorresl <jtorresl79c@gmail.com>
2024-01-18 20:36:32 -0800

    151. Adding Styling

--- components/ManageExpense/ExpenseForm.jsx
+++ components/ManageExpense/ExpenseForm.jsx
@@ -1,4 +1,4 @@
-import { View } from "react-native"
+import { StyleSheet, View } from "react-native"
 import Input from "./Input"
 
 function ExpenseForm(){
@@ -12,9 +12,14 @@ function ExpenseForm(){
         }}/>
         <Input label="Date" textInputConfig={{
             placeholder: 'YYYY-MM-DD',
-            maxLength: 10
+            maxLength: 10,
+            onChangeText: () => {}
         }}/>
-        <Input label="Description"/>
+        <Input label="Description" textInputConfig={{
+            multiline: true,
+            // autoCapitalize: 'none', 
+            // autoCorrect: false // default is true
+        }}/>
     </View>
 }
 
