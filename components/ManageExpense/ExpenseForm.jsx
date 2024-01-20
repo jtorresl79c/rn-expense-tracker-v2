628d567c60a8f0c4b8a481b592693bc3240869ab
jtorresl <jtorresl79c@gmail.com>
2024-01-19 23:22:25 -0800

    153. Handling User Input in a Generic Way

--- components/ManageExpense/ExpenseForm.jsx
+++ components/ManageExpense/ExpenseForm.jsx
@@ -1,18 +1,37 @@
 import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
+import { useState } from "react";
 
 function ExpenseForm() {
-  function amountChangeHandler() {}
+
+  const [inputValues, setInputValues] = useState({
+    amount: '',
+    date: '',
+    description: ''
+  })
+
+  function inputChangeHandler(inputIdentifier, enteredValue){
+    setInputValues( currValues => {
+      return {
+        ...currValues,
+        [inputIdentifier]: enteredValue
+      }
+    } )
+  }
 
   return (
     <View style={styles.form}>
       <Text style={styles.title}>Your expense</Text>
+      <Text>
+        {inputValues.description}
+      </Text>
       <View style={styles.inputsRow}>
         <Input
           label="Amount"
           textInputConfig={{
             keyboardType: "decimal-pad",
-            onChangeText: amountChangeHandler,
+            onChangeText: inputChangeHandler.bind(this, 'amount'),
+            value: inputValues.amount
           }}
           style={styles.rowInput}
         />
@@ -21,7 +40,8 @@ function ExpenseForm() {
           textInputConfig={{
             placeholder: "YYYY-MM-DD",
             maxLength: 10,
-            onChangeText: () => {},
+            onChangeText: inputChangeHandler.bind(this, 'date'),
+            value: inputValues.date
           }}
           style={styles.rowInput}
         />
@@ -33,6 +53,8 @@ function ExpenseForm() {
           multiline: true,
           // autoCapitalize: 'none',
           // autoCorrect: false // default is true
+          onChangeText: inputChangeHandler.bind(this, 'description'),
+          value: inputValues.description
         }}
       />
     </View>
